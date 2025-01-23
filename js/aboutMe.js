8f6e4d21914f6d9fbdd5998ad11b19783958e11b
Harsha Vardhan Madhamanchi <HarshaVardhanMadhamanchi@fico.com>
2025-01-23 16:02:29 +0530

    Mobile screen compatibility introducing

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -3,24 +3,18 @@ let slideIndex = 1;
 let isSetIntervelOn = false;
 window.onload = function()
 { 
-  console.log("triggered");
   showSlides()
   setInterval(showSlides,5000);
-  
 }
 function showSlides(n) {
-
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  console.log("slide Index  : "+slideIndex);
   if (n > slides.length || slideIndex > slides.length) {slideIndex = 1;}
   if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  console.log("test inedx : "+slideIndex);
   slideIndex = slideIndex % 3;
   slideIndex++;
-  console.log("EOF : "+slideIndex);
 }
--- js/index.js
+++ js/index.js
@@ -1,16 +1,44 @@
 var isOpen = false;
+document.addEventListener('click', function(event) {
+  const mySidebar = document.getElementById('mySidebar');
+  const openbtn = document.querySelector('.openbtn');
+  const iFrame = document.getElementById('main-content');
+  //const iframeDoc = iFrame.contentDocument || iFrame.contentWindow.document;
+
+  // Check if the click was outside the 'myDiv' element
+  if (!mySidebar.contains(event.target) && !openbtn.contains(event.target) && !iframeDoc.contains(event.target)) {
+    console.log('hello');
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("header").style.marginLeft= "0";
+    isOpen=false;
+  }
+});
 function openOrCloseNav() {
+  var screenWidth = window.innerWidth;
+    var valueToPass;
+
+    // Check screen size and set different values
+    if (screenWidth <= 600) {
+        valueToPass = "Mobile View";
+    } else if (screenWidth <= 1024) {
+        valueToPass = "Tablet View";
+    }
     isOpen = !isOpen
     console.log(isOpen)
-    if(isOpen){
-    document.getElementById("mySidebar").style.width = "16vw";
-    document.getElementById("main").style.marginLeft = "16vw";
-    document.getElementById("main-content").style.marginLeft = "0vw";
+    if(isOpen & screenWidth <= 600){
+    document.getElementById("mySidebar").style.width = "12em";
+    document.getElementById("header").style.marginLeft = "12em";
+    document.getElementById("header").style.transition = "12em";
+    //document.getElementById("main-content").style.marginLeft = "0vw";
+  } else if (isOpen & screenWidth <= 1024) {
+    document.getElementById("mySidebar").style.width = "20em";
+    document.getElementById("header").style.marginLeft = "20em";
+    document.getElementById("header").style.transition = "20em";
   }
   else {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("main-content").style.marginLeft = "10vw";
+    document.getElementById("header").style.marginLeft= "0";
+    //document.getElementById("main-content").style.marginLeft = "10vw";
   }
 }
 
@@ -19,6 +47,13 @@ function onClickOfTechStack(html){
   SkillsHTML.setAttribute('src', "https://madhamanchiharsha.github.io/pages/skills.html");
   var header = document.getElementById("page-title").innerHTML = "Exposure on Technologies";
 }
+
+function onClickOfContact(html){
+  var SkillsHTML = document.getElementById("main-content");
+  SkillsHTML.setAttribute('src', "./pages/contact.html");
+  var header = document.getElementById("page-title").innerHTML = "You can reach out to me &#128516;";
+}
+
 
 function onClickAbout(){
   var SkillsHTML = document.getElementById("main-content");
