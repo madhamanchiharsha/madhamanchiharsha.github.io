a4d3b12a3ee82be8d979b554bc56b756f9551f65
Harsha Vardhan Madhamanchi <HarshaVardhanMadhamanchi@fico.com>
2025-01-23 22:39:04 +0530

    fix

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 var isOpen = false;
+window.addEventListener('message', function(event) {
+  if (event.data === 'clickInsideIframe') {
+    // You can handle the click event here
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("openbtn").style.marginLeft= "0";
+    isOpen=false;
+
+  }
+});
 document.addEventListener('click', function(event) {
   const mySidebar = document.getElementById('mySidebar');
-  const openbtn = document.querySelector('.openbtn');
-  const iFrame = document.getElementById('main-content');
-  // const iframeDoc = iFrame.contentDocument || iFrame.contentWindow.document;
-
+  const openbtn = document.getElementById('openbtn');
   // Check if the click was outside the 'myDiv' element
-  if (!mySidebar.contains(event.target) && !openbtn.contains(event.target) && iFrame.contains(event.target)) {
-    console.log('hello');
+  if (!mySidebar.contains(event.target) && !openbtn.contains(event.target)) {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("openbtn").style.marginLeft= "0";
     isOpen=false;
@@ -15,24 +20,17 @@ document.addEventListener('click', function(event) {
 });
 function openOrCloseNav() {
   var screenWidth = window.innerWidth;
-    var valueToPass;
-
     isOpen = !isOpen
-    console.log(screenWidth,valueToPass,isOpen)
     if(isOpen & screenWidth <= 600){
     document.getElementById("mySidebar").style.width = "12em";
     document.getElementById("openbtn").style.marginLeft = "9em";
-    //document.getElementById("openbtn").style.transition = "12em";
-    //document.getElementById("main-content").style.marginLeft = "0vw";
   } else if (isOpen & screenWidth >= 1024) {
     document.getElementById("mySidebar").style.width = "20em";
     document.getElementById("openbtn").style.marginLeft = "15.5em";
-    //document.getElementById("openbtn").style.transition = "";
   }
   else {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("openbtn").style.marginLeft= "0";
-    //document.getElementById("main-content").style.marginLeft = "10vw";
   }
 }
 
@@ -42,6 +40,7 @@ function onClickOfTechStack(html){
   var header = document.getElementById("page-title").innerHTML = "Exposure on Technologies";
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("openbtn").style.marginLeft= "0";
+  isOpen = false
 
 }
 
@@ -51,7 +50,7 @@ function onClickOfContact(html){
   var header = document.getElementById("page-title").innerHTML = "You can reach out to me &#128516;";
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("openbtn").style.marginLeft= "0";
-
+  isOpen = false
 }
 
 
@@ -61,5 +60,5 @@ function onClickAbout(){
   var header = document.getElementById("page-title").innerHTML = "About Harsha Madhamanchi";
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("openbtn").style.marginLeft= "0";
-
+  isOpen = false
 }
